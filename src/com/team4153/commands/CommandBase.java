7c2812a7b32fc52e770aec91c859462f33e864ae
kirk <kirk@64.106.49.18>
2013-09-07 22:46:47 -0600

    getting command-based code together. Need to commit to test switching branch to master

---
+++ src/com/team4153/commands/CommandBase.java
@@ -0,0 +1,42 @@
+package com.team4153.commands;
+
+import com.team4153.OI;
+//import com.team4153.subsystems.Chassis;
+//import com.team4153.subsystems.GyroSensor;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The base for all commands. All atomic commands should subclass CommandBase.
+ * CommandBase creates and stores each control system. To access a
+ * subsystem elsewhere in your code in your code use CommandBase.xxx
+ * 
+ * @author KPT
+ */
+public abstract class CommandBase extends Command {
+
+    public static OI oi;
+    // Create a single static instance of all of your subsystems here
+//    public static Chassis chassis = new Chassis();
+//    public static GyroSensor gyro = new GyroSensor();
+
+    public static void init() {
+        // This MUST be here. If the OI creates Commands (which it very likely
+        // will), constructing it during the construction of CommandBase (from
+        // which commands extend), subsystems are not guaranteed to be instantiated
+        // yet. Thus, their requires() statements may grab null pointers. Bad
+        // news. Don't move it.
+        oi = new OI();
+
+        // Show what command your subsystem is running on the SmartDashboard
+        SmartDashboard.putData(chassis);
+    }
+
+    public CommandBase(String name) {
+        super(name);
+    }
+
+    public CommandBase() {
+        super();
+    }
+}
---
+++ src/com/team4153/commands/DriveWithJoystick.java
@@ -0,0 +1,43 @@
+
+package com.team4153.commands;
+
+/**
+ * Chassis drive command.
+ * 
+ * @author KPT
+ */
+public class DriveWithJoystick extends CommandBase {
+
+    public DriveWithJoystick() {
+        // Use requires() here to declare subsystem dependencies
+	requires(chassis);
+//	requires(gyro);
+	System.out.println("** DriveWithJoystick const");
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+	chassis.mecanumDrive(oi.getJoystick(), gyro.getAngle());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+	return (!oi.getTriggerButton().get());
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+	chassis.driveHalt();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+	end();
+    }
+}
